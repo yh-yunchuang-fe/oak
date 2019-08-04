84c817c9561ffcd1811fb73860080e853fd7b9d8
liangzhongling <mael.liang@live.com>
2019-08-04 13:12:30 +0800

    [Update] fix webpack.config

--- __build__/webpack.site.prod.js
+++ __build__/webpack.site.prod.js
@@ -1,8 +1,7 @@
 const config = require('./webpack.site.dev.js');
 const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 const UploadToQiniuWebpackPlugin = require('upload-to-qiniu-webpack-plugin');
-const qiniu = require('./qiniu.js');
-
+const qiniu = require('./qiniu.js').default;
 delete config.devServer;
 
 // config.plugins.push(new BundleAnalyzerPlugin());
