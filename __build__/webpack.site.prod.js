514cf01d8cabbee0316df9e3239ba968e4b98ee5
Mael.Liang <mael.liang@live.com>
2019-08-06 20:59:39 +0800

    [Update] docs

--- __build__/webpack.site.prod.js
+++ __build__/webpack.site.prod.js
@@ -1,8 +1,8 @@
-const config = require('./webpack.site.dev.js');
-const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
-const UploadToQiniuWebpackPlugin = require('upload-to-qiniu-webpack-plugin');
-const qiniu = require('./qiniu.js').default;
-delete config.devServer;
+const config = require('./webpack.site.dev.js')
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin
+const UploadToQiniuWebpackPlugin = require('upload-to-qiniu-webpack-plugin')
+const qiniu = require('./qiniu.js').default
+delete config.devServer
 
 // config.plugins.push(new BundleAnalyzerPlugin());
 config.plugins.push(new UploadToQiniuWebpackPlugin({
@@ -11,8 +11,8 @@ config.plugins.push(new UploadToQiniuWebpackPlugin({
     qiniuBucket: 'yhcms',
     qiniuZone: 'Zone_z0',
     uploadLogPath: __dirname,
-}));
+}))
 
 module.exports = Object.assign(config, {
     mode: 'production',
-});
+})
