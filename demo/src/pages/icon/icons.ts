680e971b18083390533868319add3805c0fb28af
feng.gao <gaofeng03@163.com>
2019-10-23 16:12:20 +0800

    feat: yh icon

--- demo/src/pages/icon/icons.ts
+++ demo/src/pages/icon/icons.ts
@@ -1,52 +1,332 @@
 const icons: object[] = [
     {
-        'font_class': 'brush'
-    },{
-        'font_class': 'collection_fill'
-    },{
-        'font_class': 'manage_fill'
-    },{
-        'font_class': 'manage'
-    },{
-        'font_class': 'more'
-    },{
-        'font_class': 'picture_fill'
-    },{
-        'font_class': 'picture'
-    },{
-        'font_class': 'select'
-    },{
-        'font_class': 'task'
-    },{
-        'font_class': 'workbench_fill'
-    }, {
-        'font_class': 'workbench'
-    },{
-        'font_class': 'down'
-    }, {
-        'font_class': 'brightness'
-    }, {
-        'font_class': 'Privacy'
-    }, {
-        'font_class': 'save'
-    }, {
-        'font_class': 'collection'
-    }, {
-        'font_class': 'upward'
-    }, {
-        'font_class': 'night'
-    }, {
-        'font_class': 'next'
-    }, {
-        'font_class': 'return'
-    }, {
-        'font_class': 'selected'
-    }, {
-        'font_class': 'unselect'
-    }, {
-        'font_class': 'select-disable'
-    }, {
-        'font_class': 'close-fill'
+        font_class: 'home'
+    },
+    {
+        font_class: 'classification'
+    },
+    {
+        font_class: 'shopping-cart'
+    },
+    {
+        font_class: 'member-center'
+    },
+    {
+        font_class: 'search'
+    },
+    {
+        font_class: 'trash'
+    },
+    {
+        font_class: 'share'
+    },
+    {
+        font_class: 'edit'
+    },
+    {
+        font_class: 'address'
+    },
+    {
+        font_class: 'camera'
+    },
+    {
+        font_class: 'delivery'
+    },
+    {
+        font_class: 'refund'
+    },
+    {
+        font_class: 'goods'
+    },
+    {
+        font_class: 'invoice'
+    },
+    {
+        font_class: 'order'
+    },
+    {
+        font_class: 'member'
+    },
+    {
+        font_class: 'praise'
+    },
+    {
+        font_class: 'trample'
+    },
+    {
+        font_class: 'multiple-choice-line'
+    },
+    {
+        font_class: 'phone'
+    },
+    {
+        font_class: 'close'
+    },
+    {
+        font_class: 'question'
+    },
+    {
+        font_class: 'exclamation-point'
+    },
+    {
+        font_class: 'plus-sign'
+    },
+    {
+        font_class: 'minus'
+    },
+    {
+        font_class: 'location'
+    },
+    {
+        font_class: 'cyclic-left'
+    },
+
+    {
+        font_class: 'cyclic-right'
+    },
+
+    {
+        font_class: 'cyclic-up'
+    },
+
+    {
+        font_class: 'cyclic-down'
+    },
+
+    {
+        font_class: 'image'
+    },
+
+    {
+        font_class: 'scan-code'
+    },
+
+    {
+        font_class: 'member-code'
+    },
+
+    {
+        font_class: 'recharge'
+    },
+
+    {
+        font_class: 'meal'
+    },
+
+    {
+        font_class: 'estimate-line'
+    },
+
+    {
+        font_class: 'filtrate'
+    },
+
+    {
+        font_class: 'trumpet'
+    },
+
+    {
+        font_class: 'refresh'
+    },
+
+    {
+        font_class: 'calendar'
+    },
+
+    {
+        font_class: 'flashlight-line'
+    },
+
+    {
+        font_class: 'left-light'
+    },
+
+    {
+        font_class: 'down-light'
+    },
+
+    {
+        font_class: 'up-light'
+    },
+
+    {
+        font_class: 'left-bold'
+    },
+
+    {
+        font_class: 'right-bold'
+    },
+
+    {
+        font_class: 'down-bold'
+    },
+
+    {
+        font_class: 'up-bold'
+    },
+
+    {
+        font_class: 'pulldown-bold'
+    },
+
+    {
+        font_class: 'choice'
+    },
+
+    {
+        font_class: 'close-light'
+    },
+
+    {
+        font_class: 'plus-sign1'
+    },
+
+    {
+        font_class: 'minus1'
+    },
+
+    {
+        font_class: 'more'
+    },
+
+    {
+        font_class: 'home-facet'
+    },
+
+    {
+        font_class: 'classification-facet'
+    },
+
+    {
+        font_class: 'shopping-cart-facet'
+    },
+
+    {
+        font_class: 'member-center-facet'
+    },
+
+    {
+        font_class: 'praise-facet'
+    },
+
+    {
+        font_class: 'trample-facet'
+    },
+
+    {
+        font_class: 'choice-facet'
+    },
+
+    {
+        font_class: 'plus-sign-facet'
+    },
+
+    {
+        font_class: 'close-facet'
+    },
+
+    {
+        font_class: 'left-facet'
+    },
+
+    {
+        font_class: 'triangle-down'
+    },
+
+    {
+        font_class: 'triangle-up'
+    },
+
+    {
+        font_class: 'wallet'
+    },
+
+    {
+        font_class: 'bag'
+    },
+
+    {
+        font_class: 'estimate-facet'
+    },
+
+    {
+        font_class: 'refund-facet'
+    },
+
+    {
+        font_class: 'orders'
+    },
+
+    {
+        font_class: 'rank'
+    },
+
+    {
+        font_class: 'VIP'
+    },
+
+    {
+        font_class: 'address-facet'
+    },
+
+    {
+        font_class: 'flashlight-facet'
+    },
+
+    {
+        font_class: 'filtrate-facet'
+    },
+
+    {
+        font_class: 'gift'
+    },
+
+    {
+        font_class: 'activity'
+    },
+
+    {
+        font_class: 'new-species'
+    },
+
+    {
+        font_class: 'address-management'
+    },
+
+    {
+        font_class: 'feedback'
+    },
+
+    {
+        font_class: 'shop'
+    },
+
+    {
+        font_class: 'service'
+    },
+
+    {
+        font_class: 'set'
+    },
+
+    {
+        font_class: 'label'
+    },
+
+    {
+        font_class: 'fire'
+    },
+
+    {
+        font_class: 'rocket'
+    },
+
+    {
+        font_class: 'card'
+    },
+
+    {
+        font_class: 'right-light'
     }
 ]
 
