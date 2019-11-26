8c03b75742638263af2ef9d424b2eb0ec64412aa
wangxudong <80913144@yonghui.cn>
2019-11-26 11:31:41 +0800

    feat: 新增 search bar 搜索框

--- docs/src/doc.config.js
+++ docs/src/doc.config.js
@@ -42,6 +42,9 @@ export default {
             }, {
                 name: 'Switch 开关',
                 path: '/Switch',
+            }, {
+                name: 'Search Bar 搜索框',
+                path: '/SearchBar',
             }, ]
         },{
             name: '展示',
