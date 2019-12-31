be7a06bea56a966888832d15a8aed0503fb29b95
Mael.Liang <mael.liang@live.com>
2019-12-31 14:55:01 +0800

    [FEA] add TreeSelect component

--- demo/src/pages/treeSelect/treeSelect.ts
+++ demo/src/pages/treeSelect/treeSelect.ts
@@ -1,6 +1,50 @@
 //index.js
 //获取应用实例
 Page({
+    data: {
+        treeData: [{
+            label: 'Node1',
+            value: '0-0',
+            key: '0-0',
+            children: [{
+                label: 'Node1 Child1',
+                value: '0-0-1',
+                key: '0-0-1',
+                disabled: true,
+            }, {
+                label: 'Node1 Child2',
+                value: '0-0-2',
+                key: '0-0-2',
+            }],
+        }, {
+            label: 'Node2',
+            value: '0-0',
+            disabled: true,
+            key: '0-0',
+            children: [{
+                label: 'Child 2 fadfadfsa',
+                value: '0-0-1',
+                key: '0-0-1',
+            }, {
+                label: 'Child Node2 fdsafsad',
+                value: '0-0-2',
+                key: '0-0-2',
+            }],
+        }, {
+            label: 'Node3',
+            value: '0-0',
+            key: '0-0',
+            children: [{
+                label: 'Child Node1 fadsfadsfdas',
+                value: '0-0-1',
+                key: '0-0-1',
+            }, {
+                label: 'Child Node2 wwwww',
+                value: '0-0-2',
+                key: '0-0-2',
+            }],
+        }],
+    },
     onShareAppMessage(): object {
         return {
             title: 'Oak Weui',
--- src/TreeSelect/tree-select.ts
+++ src/TreeSelect/tree-select.ts
@@ -2,18 +2,51 @@ import BasicBehavior from 'Mixins/basic'
 Component({
     behaviors: [BasicBehavior],
     properties: {
-        // 这里定义了innerText属性，属性值可以在组件使用时指定
-        innerText: {
+        treeData: {
+            type: Array,
+            value: [],
+        },
+        labelWidth: {
             type: String,
-            value: 'default value',
-            optionalTypes: [String],
-        }
+            value: ''
+        },
+        defaultLabelIndex: {
+            type: Number,
+            value: null,
+            optionalTypes: [String]
+        },
+        labelIndex: {
+            type: Number,
+            value: null,
+            optionalTypes: [String]
+        },
     },
-    data: {
-        // 这里是一些组件内部数据
-        someData: {}
+    externalClasses: ['label-class', 'label-item-class', 'label-active-class', 'content-class', 'item-disabled-class', 'item-class'],
+    attached(): void {
+        this.hasSetLabelIndex = this.data.labelIndex !== null
+    },
+    ready(): void {
+        this.setData({
+            labelIndex: this.data.defaultLabelActive || 0,
+        })
     },
     methods: {
+        onLabelTap(event: event): void {
+            const { index } = event.currentTarget.dataset
+            const { treeData } = this.data
 
+            if (treeData[index].disabled) return
+
+            // 如果外部没有设定 `labelIndex` 则主动修改，否则等外部处理
+            if (!this.hasSetLabelIndex) {
+                this.setData({
+                    labelIndex: index,
+                })
+            }
+            this.triggerEvent('labeltap', { index })
+        },
+        onContentItemTap(event: event): void {
+            console.log(event)
+        }
     }
 })
