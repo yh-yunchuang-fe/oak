acafac9392fdfc7e1f3fec6afa86e105f01d9b06
feng.gao <gaofeng03@163.com>
2021-04-02 15:29:43 +0800

    feat: progress

--- dist/Progress/progress.js
+++ dist/Progress/progress.js
@@ -1,22 +1,19 @@
+"use strict";
 Component({
     properties: {
         radiu: {
-            // 设置进度条圆角
             type: Number,
             value: 12
         },
         color: {
-            // 已选择的进度条颜色
             type: String,
             value: '#FF3819'
         },
         bgColor: {
-            // 未选择的进度条颜色
             type: String,
             value: '#ff38194d'
         },
         strokeWidth: {
-            // 已选择百分比
             type: String,
             value: 0
         },
@@ -29,7 +26,6 @@ Component({
             value: '#ddd'
         },
         disabled: {
-            // 置灰状态
             type: Boolean,
             value: false
         },
@@ -41,4 +37,4 @@ Component({
     externalClasses: ['ext-class', 'bg-class', 'color-class', 'disabled-bg-class', 'disabled-color-class'],
     data: {},
     methods: {}
-})
+});
