c2d8c5e93e307084e5af57d5a4ef016842f65582
fangcw1 <fangcw1@yonghui.cn>
2021-12-03 20:08:04 +0800

    feat: Button按钮-v1

--- src/Button/button.ts
+++ src/Button/button.ts
@@ -1,59 +1,81 @@
-import BasicBehavior from 'Mixins/basic'
-import OpenTypeBehavior from 'Mixins/open-type'
-import ButtonBehavior from 'Mixins/button'
+import BasicBehavior from '../mixins/basic'
+import OpenTypeBehavior from '../mixins/open-type'
+import ButtonBehavior from './button-mixin'
 
 Component({
     behaviors: [BasicBehavior, OpenTypeBehavior, ButtonBehavior],
     properties: {
-        size: {
-            type: String,
-            value: 'default',
-            optionalTypes: [String],
-        },
-        type: {
-            type: String,
-            value: 'default',
-            optionalTypes: [String],
-        },
-        shape: {
-            type: String,
-            value: ''
-        },
-        icon: {
-            type: String,
-            value: ''
-        },
-        plain: {
-            type: Boolean,
-            value: false,
-        },
-        loading: {
-            type: Boolean,
-            value: false,
-        },
-        loaderColor: {
-            type: String,
-            value: ''
-        },
-        loaderSize: {
-            type: String,
-            value: ''
-        },
-        disabled: {
-            type: Boolean,
-            value: false,
-        },
-        height: {
-            type: String,
-            value: null,
-        },
-        width: {
-            type: String,
-            value: null,
-        },
+        // 按钮的颜色
         color: {
             type: String,
-            value: null,
+            value: 'default', // 'default' | 'primary'
+        },
+        // 填充模式
+        fill: {
+            type: String,
+            value: 'solid',// 'solid' | 'outline' | 'none'
+        },
+        // 大小
+        size: {
+            type: String,
+            value: 'middle', // 'mini' | 'small' | 'middle' | 'large'
+        },
+        // 是否是块级元素
+        block: {
+            type: Boolean,
+            value: false
+        },
+        // 按钮形状-圆角
+        round: {
+            type: Boolean,
+            value: false
+        },
+        // 按钮形状-直角
+        angle: {
+            type: Boolean,
+            value: false
+        },
+        // // 按钮形状-胶囊（默认）
+        // capsules: {
+        //     type: Boolean,
+        //     value: false
+        // },
+        // 是否是待唤起状态
+        waiting: {
+            type: Boolean,
+            value: false
+        },
+        // 是否是块级元素
+        disabled: {
+            type: Boolean,
+            value: false
+        },
+        // 是否处于加载状态
+        loading: {
+            type: Boolean,
+            value: false
+        },
+        // 加载状态下额外展示的文字
+        loadingText: {
+            type: String,
+            value: '',
+        },
+        // 图标
+        icon: {
+            type: String,
+            value: '',
+        },
+    },
+    data: {
+        loadingColor: ''
+    },
+    lifetimes: {
+        ready(): void {
+            this.setLoadingColor()
+        }
+    },
+    methods: {
+        setLoadingColor(): void {
         }
     },
     externalClasses: ['button-class', 'hover-class', 'loading-class'],
