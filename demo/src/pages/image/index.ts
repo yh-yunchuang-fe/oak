cb5aa925f9a8f820561d7433b533cc28f098c04a
fangcw1 <fangcw1@yonghui.cn>
2021-12-06 18:22:42 +0800

    feat: 图片组件- image- v1

--- demo/src/pages/image/index.ts
+++ demo/src/pages/image/index.ts
@@ -7,4 +7,10 @@ Page({
             path: '/pages/Image/image',
         }
     },
+    data: {
+        img1: 'https://image.yonghuivip.com/image/1638171083730b3ecb3b5f0885edd7f5a7b5310c89568a9253633.jpg',
+        img2: 'https://image.yonghuivip.com/image/16303891639179b6d5ba3ae7908c8fa574c8d6b4347cac4ee0a2a.jpg',
+        img3: 'http://image.yonghuivip.com/image/1638260711605a05ea34e9e8edefc273b751d482b93c330bb5748.jpg',
+        errImg: 'http://image.yonghuivip.com/image/1638260711605a05ea34e9e8edefc273b751d482b93c330bb57'
+    }
 })
