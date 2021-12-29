17d12698a50821569ec9193ca23e6984c68bda41
fangcw1 <fangcw1@yonghui.cn>
2021-12-29 20:06:14 +0800

    feat: Tag-v2

--- src/Tag/tag.ts
+++ src/Tag/tag.ts
@@ -31,7 +31,7 @@ Component({
         //图标 位置
         iconPosition: {
             type: String,
-            value: 'left', // left | right
+            value: 'before', // before | after
         },
         //图标 样式
         iconStyle: {
@@ -50,39 +50,6 @@ Component({
         }
     },
     methods: {
-        _setStyle(): void {
-            const { color, textColor, plain, borderColor } = this.data
-            let _style = ''
-            const colorRegx = /^(#(?:[0-9a-f]{2}){2,4}|#[0-9a-f]{3}|(?:rgba?|hsla?)\((?:\d+%?(?:deg|rad|grad|turn)?(?:,|\s)+){2,3}[\s\/]*[\d\.]+%?\))/i
-
-            if (plain) {
-                _style = `;border: 2rpx solid ${color};`
-                !textColor && (_style += `;color: ${color};`)
-            } else if (colorRegx.test(color)) {
-                const regx = /^#(?:[0-9a-f]{2}){2,4}|#[0-9a-f]{3}/i
-
-                if (regx.test(color)) {
-                    const colorArray: string[] = color.split(',')
-                    _style += colorArray.length === 2 ? `;background:linear-gradient(${this.data.rotate}deg, ${colorArray[0]}, ${colorArray[1]});` : `;background:${colorArray[0]};`
-                } else {
-                    _style += `;background:${color};`
-                }
-
-                !textColor && (_style += ';color: #fff;')
-            }
-
-            if (textColor) {
-                _style += `;color: ${textColor};`
-            }
-
-            if (borderColor) {
-                _style += `;border: 2rpx solid ${borderColor};`
-            }
-
-            this.setData({
-                _style,
-            })
-        }
     },
-    externalClasses: ['custom-class', 'before-class', 'after-class'],
+    externalClasses: ['ext-class', 'before-class', 'after-class'],
 })
