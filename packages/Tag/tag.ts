89a15038af7107d7f7e52a6ae0987e691bff5aca
方承维 <fangcw1@yonghui.cn>
2022-03-04 20:30:19 +0800

    feat: packages tag

--- packages/Tag/tag.ts
+++ packages/Tag/tag.ts
@@ -3,84 +3,58 @@ import BasicBehavior from '../mixins/basic'
 Component({
     behaviors: [BasicBehavior],
     properties: {
+        //字体颜色
         color: {
             type: String,
-            value: 'default',
-            optionalTypes: [String],
-            observer(color): void {
-                color && this._setStyle()
-            }
+            value: '',
         },
-        rotate: {
-            type: Number,
-            value: 90
+        // 预设值
+        preset: {
+            type: String,
+            value: 'red',
         },
-        plain: {
-            type: Boolean,
-            value: false,
-            optionalTypes: [String],
-        },
-        shape: {
+        // 背景色 支持渐变
+        background: {
             type: String,
             value: '',
         },
-        textColor: {
-            type: String,
-            value: '',
-            observer(color): void {
-                color && this._setStyle()
-            }
-        },
+        // 边框颜色 默认不显示
         borderColor: {
             type: String,
             value: '',
-            observer(color): void {
-                color && this._setStyle()
-            }
         },
-        beforeImage: {
+        // 是否空心
+        plain: {
+            type: Boolean,
+            value: false,
+        },
+        // 前后图标 支持图片
+        icon: {
             type: String,
             value: null,
         },
-        afterImage: {
+        //图标 位置
+        iconPosition: {
             type: String,
-            value: null,
+            value: 'before', // before | after
+        },
+        //图标 样式
+        iconStyle: {
+            type: String,
+            value: '', 
+        },
+        // 圆角
+        radius: {
+            type: String,
+            value: '',
+        },
+
+        disabled:{
+            type: Boolean,
+            value: false,
         }
     },
     methods: {
-        _setStyle(): void {
-            const { color, textColor, plain, borderColor } = this.data
-            let _style = ''
-            const colorRegx = /^(#(?:[0-9a-f]{2}){2,4}|#[0-9a-f]{3}|(?:rgba?|hsla?)\((?:\d+%?(?:deg|rad|grad|turn)?(?:,|\s)+){2,3}[\s\/]*[\d\.]+%?\))/i
-
-            if (plain) {
-                _style = `;border: 2rpx solid ${color};`
-                !textColor && ( _style += `;color: ${color};`)
-            } else if (colorRegx.test(color)) {
-                const regx = /^#(?:[0-9a-f]{2}){2,4}|#[0-9a-f]{3}/i
-
-                if (regx.test(color)) {
-                    const colorArray: string[] = color.split(',')
-                    _style += colorArray.length === 2 ? `;background:linear-gradient(${this.data.rotate}deg, ${colorArray[0]}, ${colorArray[1]});` : `;background:${colorArray[0]};`
-                } else {
-                    _style += `;background:${color};`
-                }
-
-                !textColor && ( _style += ';color: #fff;')
-            }
-
-            if (textColor) {
-                _style += `;color: ${textColor};`
-            }
-
-            if (borderColor) {
-                _style += `;border: 2rpx solid ${borderColor};`
-            }
-
-            this.setData({
-                _style,
-            })
-        }
     },
-    externalClasses: ['custom-class', 'before-class', 'after-class'],
+    externalClasses: ['ext-class', 'before-class', 'after-class'],
 })
