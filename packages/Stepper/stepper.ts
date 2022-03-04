d8dd0b62a960ac5ec549e4a781b3980929733750
方承维 <fangcw1@yonghui.cn>
2022-03-04 20:25:43 +0800

    feat: packages stepper

--- packages/Stepper/stepper.ts
+++ packages/Stepper/stepper.ts
@@ -1,204 +1,138 @@
+
+const add = (num1: number, num2: number): number => {
+    const cardinal = 10 ** 10
+    return Math.round((num1 + num2) * cardinal) / cardinal
+}
+
 Component({
     properties: {
-        value: {   // stepper 步进器 value
+        // value
+        value: {
             type: Number || String,
-            value: null,
+            value: 0,
             observer(newValue): void {
                 this.setData({ _value: newValue })
-                this.isDisabled()
             },
+        },
+        // 类型: 'default' | 'primary'
+        type: {
+            type: String,
+            value: 'primary',
+        },
+        // 尺寸: small ｜ large
+        size: {
+            type: String,
+            value: 'small'
         },
         min: {
             type: Number,
             value: null,
-            observer(nval, oval): void {
-                if (nval !== oval) {
-                    this.isDisabled()
-                }
-            }
         },
         max: {
             type: Number,
             value: null,
-            observer(nval, oval): void {
-                if (nval !== oval) {
-                    this.isDisabled()
-                }
-            }
         },
         step: {
             type: Number,
             value: 1
         },
-        enableInput: { // 使能输入
+        // 是否允许键盘输入
+        enableInput: {
             type: Boolean,
             value: false
         },
-        integer: {
-            type: Boolean,  // 键盘是否包含小数点
-            value: false
-        },
-        disabled: { // 禁止整个模块点击
+        // 是否禁用
+        disabled: {
             type: Boolean,
             value: false
         },
-        disabledOpacity: { // 模块禁止灰度
-            type: Number,
-            value: 0.4
-        },
+        // 异步修改
         asyncChange: {
             type: Boolean,
             value: false
         },
-        decimalLength: { // 小数点长度
+        // 小数点长度
+        decimalLength: {
             type: Number,
             value: null
         },
-        height: {  // 设置模块的height
-            type: Number,
-            value: null
+        // 自定义icon
+        icons: {
+            type: Array,
+            value: []
         },
-        activeColor: {
-            type: String,
-            value: '#FD7622'
-        },
-        minusActiveColor: {
-            type: String,
-            value: ''
-        },
-        plusActiveColor: {
-            type: String,
-            value: ''
-        },
-        disableColor: {
-            type: String,
-            value: '#333333'
-        },
-        iconSize: {
-            type: Number,
-            value: 14
-        },
-        stepperStyle: {   // 步进器样式
-            type: String,
-            value: ''
-        },
-        minusStyle: {  // minus 样式
-            type: String,
-            value: ''
-        },
-        inputStyle: {   // 设置输入框的样式
-            type: String,
-            value: ''
-        },
-        plusStyle: { // 设置plus样式
-            type: String,
-            value: ''
-        }
     },
     externalClasses: ['ext-class', 'minus-class', 'plus-class', 'input-class'],
     data: {
-        disabledPlus: false,
-        disabledMinus: false,
         _value: ''
     },
     methods: {
-        isDisabled(type): boolean {
-            const { min, max, disabled, _value } = this.data
-            const disabledPlus = disabled || (typeof max === 'number' && _value >= max)
-            const disabledMinus = disabled || (typeof min === 'number' && _value <= min)
-            disabledPlus !== this.data.disabledPlus && this.setData({
-                disabledPlus,
-            })
-            disabledMinus !== this.data.disabledMinus && this.setData({
-                disabledMinus,
-            })
-            if (type === 'plus') {
+        changeValue(value): void {
+            const _value = this.range(value)
+            // 异步改变 需在组件外面修改value值
+            this.triggerEvent('onChange', _value)
+            if (this.data.asyncChange) return
 
-                return disabledPlus
-            }
-
-            return disabledMinus
-        },
-        add(num1: number, num2: number): number {
-            const cardinal = 10 ** 10
-            return Math.round((num1 + num2) * cardinal) / cardinal
-        },
-        onBlur(): void {
-            const _value = this.range(this.data._value)
             this.setData({
-                _value,
+                _value
             })
         },
+        onChange(type: string): void {
+            const diff = type === 'minus' ? -this.data.step : +this.data.step
+            const _value = add(+this.data._value, diff)
+            this.changeValue(_value)
+        },
+        onMinus(e: event): void {
+            this.triggerEvent('onMinus', this.returnInfo('minus', e))
+            this.onChange('minus')
+        },
+        onPlus(e: event): void {
+            this.triggerEvent('onPlus', this.returnInfo('plus', e))
+            this.onChange('plus')
+        },
+
         // limit _value range
         range(_value): number {
             _value = String(_value).replace(/[^0-9.-]/g, '')
             // format range
             _value = _value === '' ? 0 : + _value
-            const { min, max } = this.data
+            const { min, max, decimalLength } = this.data
             if (typeof min === 'number' && _value < min) {
                 _value = min
             }
             if (typeof max === 'number' && _value > max) {
                 _value = max
             }
+            _value = decimalLength ? (_value).toFixed(decimalLength) : _value
             return _value
         },
         onInput(event): void {
+            this.triggerEvent('onInput', this.returnInfo('input', event))
             const { value } = event.detail || {}
+            const _value = this.range(value)
             this.setData({
-                _value: value,
+                _value,
             })
         },
+
         onFocus(event): void {
-            this.triggerEvent('onFocus', event)
-        },
-        onInputClick(event): void {
-            this.triggerEvent('onInputClick', event)
-        },
-        onChange(): void {
-            const { type } = this
-            if (this.isDisabled(type)) {
-                return
-            }
-            const diff = type === 'minus' ? -this.data.step : +this.data.step
-            const _value = this.add(+this.data._value, diff)
-            this.changeValue(_value)
-            this.isDisabled()
+            this.triggerEvent('onFocus', this.returnInfo('focus', event))
         },
 
-        onTap(event): void {
-            const { type } = event.currentTarget.dataset
-            const { onPlus, onMinus } = this.data
-            this.type = type
-            this.onChange()
-            const returnInfo = this.returnInfo(type, event.touches)
-            if (type === 'plus') {
-                if (typeof onPlus === 'function') {
-                    onPlus(returnInfo)
-                } else {
-                    this.triggerEvent('onPlus', returnInfo)
-                }
-                return
-            }
-
-            if (typeof onMinus === 'function') {
-                onMinus(returnInfo)
-            } else {
-                this.triggerEvent('onMinus', returnInfo)
-            }
+        onBlur(event): void {
+            this.triggerEvent('onBlur', this.returnInfo('blur', event))
         },
 
-        returnInfo(type, touches): object {
+        onConfirm(event): void {
+            this.triggerEvent('onConfirm', this.returnInfo('confirm', event))
+        },
+
+        returnInfo(type, event): object {
             const { _value, disabled, min, max, step, decimalLength } = this.data
             return {
-                value: _value, disabled, min, max, step, decimalLength, type, touches
+                value: _value, disabled, min, max, step, decimalLength, type, event
             }
         },
 
-        changeValue(_value): void {
-            this.setData({
-                _value: this.data.asyncChange ? this.data._value : _value
-            })
-        },
     }
 })
