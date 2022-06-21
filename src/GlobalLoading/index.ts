8c6648f8f5a31f01ef27e9fe037e9d80b3aecf76
KangShudong <313751672@qq.com>
2022-06-21 16:19:59 +0800

    feat: loading动效修改

--- src/GlobalLoading/index.ts
+++ src/GlobalLoading/index.ts
@@ -6,21 +6,9 @@ Component({
         styleIsolation: 'isolated'
     },
     properties: {
-        position: {
-            type: String,
-            value: 'fixed'
-        },
-        text: {
-            type: String,
-            value: ''
-        },
         isShow: {
             type: Boolean,
             value: false
-        },
-        mask: {
-            type: Boolean,
-            value: false,
         }
     }
 })
